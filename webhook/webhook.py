fb609084fc86c9e8a8b998f3416abf37072d269f
Adriano Vieira <adriano.svieira@gmail.com>
2014-07-28 19:27:35 -0300

    Inclui recursos para "debug" da aplicação
    - para debug é necessário haver ipdb (ver requirements-ipdb.txt)
    - somente executa a aplicação caso seja um "merge request"

--- webhook/webhook.py
+++ webhook/webhook.py
@@ -1,6 +1,23 @@
 from flask import Flask, request, json
 import requests
 
+
+'''
+CONTANTES
+avaliar "modulo: logging"
+'''
+DEBUG_LEVEL0 = 0
+DEBUG_LEVEL1 = 1 # mostra algumas mensagens na console
+DEBUG_INTERATIVO = 9 # ipdb ativado: "ipdb.set_trace()"
+
+
+'''
+a ser extraido para arquivo de configuracoes
+avaliar "modulo: logging"
+'''
+debug = True
+debug_level = DEBUG_LEVEL0
+
 app = Flask(__name__)
 
 @app.route('/',methods=['GET', 'POST'])
@@ -9,13 +26,23 @@ def index():
         return 'Aplicacao para webhook! \n Use adequadamente!'
 
    elif request.method == 'POST':
-         #hookdata = request.data
-        print request.data
-        #print "Dados webhook: {}".format(hookdata)
-        #print "Dados: ".format(hookdata)
-        print "Ola, adriano"
-        return "OK"
+        if debug and debug_level == DEBUG_INTERATIVO:
+           import ipdb; ipdb.set_trace() # ativado para debug interativo
+
+        hookdata = json.loads(request.data)
+        if hookdata['object_kind'] != 'merge_request':
+           print 'Aplicacao webhook para "Merge Request"! \n Use adequadamente!'
+           return '{"status": "ERROR", "message": "not a merge request"}'
+
+        print "\nProcessing merge request ...\n"
+
+        print hookdata # ['object_attributes']['source_branch']
+
+        return '{"status": "OK"}'
 
 if __name__ == '__main__':
-   app.debug = True
-   app.run()
+   if debug:
+      app.debug = True
+      app.run(host='0.0.0.0')
+   else:
+      app.run()
